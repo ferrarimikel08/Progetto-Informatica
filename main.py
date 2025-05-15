ac2fe746f52b8695ba41294fa75168748a67412c
ferrarimikel08 <mikel.ferrari@liceobanfi.eu>
2025-05-15 19:21:03 +0200

    grafica iniziale: sistemare dimensione

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame,sys
 from pygame.locals import *
+from maze import Maze
 pygame.init()
 
 BLACK = (0,0,0)
@@ -15,10 +16,20 @@ pygame.display.set_caption("Labirinto")
 clock = pygame.time.Clock()
 fps = 60
 
+cell_size = 10
+
+maze = Maze("gamemap1.txt")
+
+maze_width, maze_height = maze.get_matrix_size()
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.display.quit()
             pygame.quit()
             sys.exit()
+    maze.draw_maze(screen,maze_height,maze_width,cell_size,RED,WHITE)
+
+    pygame.display.update()
     pygame.display.flip()
+    clock.tick(60)
--- maze.py
+++ maze.py
@@ -2,7 +2,7 @@ import pygame
 
 class Maze:
 
-    def __init__(self,screen,file_name):
+    def __init__(self,file_name):
         matrix = []
         with open(file_name,"r",encoding="utf-8") as f:
             lines = f.readlines()
@@ -11,3 +11,18 @@ class Maze:
 
         self.maze = matrix
     
+    def get_matrix_size(self):
+        rows = len(self.maze)
+        cols = len(self.maze[0])
+        return (cols,rows)
+
+
+    def draw_maze(self,screen,maze_height,maze_width,cell_size,wall_color,void_color):
+        
+        for x in range(maze_height):
+            for y in range(maze_width):
+                if self.maze[x][y] == '2':
+                    pygame.draw.rect(screen,wall_color,(x*cell_size,y*cell_size,cell_size,cell_size))
+                if self.maze[x][y]== '0': 
+                    pygame.draw.rect(screen,void_color,(x*cell_size,y*cell_size,cell_size,cell_size))
+
